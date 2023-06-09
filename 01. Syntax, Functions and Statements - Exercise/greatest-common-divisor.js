572fc50fc835288efc5d0b3d827559a0d4ec86e9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-09 10:53:55 +0300

    task done

---
+++ 01. Syntax, Functions and Statements - Exercise/greatest-common-divisor.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function greatestCommonDivisor(num1, num2) {
+  let greatestDivisor = 0;
+  for (let i = 1; i <= num2; i++) {
+    if (num2 % i === 0 && num1 % i === 0) {
+      greatestDivisor = i;
+    }
+  }
+  console.log(greatestDivisor);
+}
+
+greatestCommonDivisor(15, 5);
+greatestCommonDivisor(2154, 458);
