4404586f54106be3e5d9c70acd2e08aacb4cddac
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-09 10:59:25 +0300

    task done

---
+++ 01. Syntax, Functions and Statements - Exercise/same-numbers.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function sameNumbers(number) {
+  let sum = 0;
+  let isSame = true;
+  number = String(number);
+  let firstDigit = number[0];
+  for (let i = 0; i < number.length; i++) {
+    sum += Number(number[i]);
+  }
+
+  for (let i = 1; i < number.length; i++) {
+    if (number[i] !== firstDigit) {
+      sameOrNot = false;
+    }
+  }
+  console.log(sum);
+  console.log(isSame);
+}
+
+sameNumbers(2222222);
