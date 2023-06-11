24872ea23b127f2dfdcb6190df0fc11541ce888a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-11 10:55:17 +0300

    task done

---
+++ 02. Arrays and Nested Arrays - Lab/piece-of-pie.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function pieceOfPie(pieFlavors, start, end) {
+  let startIndex = pieFlavors.indexOf(start);
+  let endIndex = pieFlavors.indexOf(end);
+  let newArray = pieFlavors.slice(startIndex, endIndex + 1);
+  console.log(newArray);
+}
+
+pieceOfPie(
+  [
+    "Pumpkin Pie",
+    "Key Lime Pie",
+    "Cherry Pie",
+    "Lemon Meringue Pie",
+    "Sugar Cream Pie",
+  ],
+  "Key Lime Pie",
+  "Lemon Meringue Pie"
+);
