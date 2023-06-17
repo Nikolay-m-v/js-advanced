7578c5dafe45db9492458fc3114606a0a082af0a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-17 11:52:36 +0300

    task done

---
+++ 03. Objects and Composition - Exercise/heroes-inventory.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function heroInventory(array) {
+  let heroes = [];
+
+  for (let line of array) {
+    let [name, level, items] = line.split(" / ");
+    level = Number(level);
+    items = items ? items.split(", ") : [];
+
+    heroes.push({ name, level, items });
+  }
+  console.log(JSON.stringify(heroes));
+}
+
+heroInventory([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1 / Desolator, Sentinel, Antara",
+]);
