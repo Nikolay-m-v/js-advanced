c619d36c087b499692178868829bf4a08f8db064
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-19 10:54:23 +0300

    task done

---
+++ 03. Objects and Composition - Exercise/heroes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function solve() {}
+
+let create = solve();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball");
+scorcher.cast("thunder");
+scorcher.cast("light");
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight();
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
