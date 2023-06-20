df59639a2c4cd56148ffc32d138af5fd9cd9a420
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-20 13:34:13 +0300

    tasks

--- 04. DOM Introduction - Lab/collect list items/collect-items.js
+++ 04. DOM Introduction - Lab/collect list items/collect-items.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function extractText() {
+  const items = Array.from(document.querySelectorAll("li"));
+
+  const result = items.map((el) => el.textContent).join("\n");
+
+  document.getElementById("result").value = result;
+}
