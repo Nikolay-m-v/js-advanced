904e418cf9e5581c238f9477ec37c81394a3439d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-20 13:36:19 +0300

    tasks

--- 04. DOM Introduction - Lab/sum numbers/sum-numbers.js
+++ 04. DOM Introduction - Lab/sum numbers/sum-numbers.js
@@ -1,3 +1,8 @@
 "use strict";
 
-function sumNumbers() {}
+function sumNumbers() {
+  const num1 = Number(document.getElementById("num1").value);
+  const num2 = Number(document.getElementById("num2").value);
+
+  document.getElementById("sum").value = num1 + num2;
+}
