b0eae527ff662b47281b62c93f6dd7847aa665eb
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-20 13:41:41 +0300

    tasks

---
+++ 04. DOM Introduction - Lab/colorize table/colorize-table.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function colorizeTable() {
+  const rows = document.querySelectorAll("tr");
+
+  for (let i = 1; i < rows.length; i += 2) {
+    rows[i].style.backgroundColor = "teal";
+  }
+}
