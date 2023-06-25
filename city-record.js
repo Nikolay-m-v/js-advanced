107e3fba06a997e73fcc31f24182cacedd13661a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-25 10:12:23 +0300

    task done

---
+++ city-record.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function cityRecord(name, population, treasury) {
+  let record = { name: name, population: population, treasury: treasury };
+
+  console.log(record);
+  return record;
+}
+
+cityRecord("Santo Domingo", 12000, 23500);
