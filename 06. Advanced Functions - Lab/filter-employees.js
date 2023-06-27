ab820c9b28ced47af078022ccd61370c65f82e89
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-27 12:35:11 +0300

    tasks

---
+++ 06. Advanced Functions - Lab/filter-employees.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function filter(data, criteria) {
+  let [criteriaWord, criteriaValue] = criteria.split("-");
+
+  JSON.parse(data).filter(employee[criteriaWord] === criteriaValue);
+}
+
+filter(
+  `[{
+  "id": "1",
+  "first_name": "Ardine",
+  "last_name": "Bassam",
+  "email": "abassam0@cnn.com",
+  "gender": "Female"
+}, {
+  "id": "2",
+  "first_name": "Kizzee",
+  "last_name": "Jost",
+  "email": "kjost1@forbes.com",
+  "gender": "Female"
+},  
+{
+  "id": "3",
+  "first_name": "Evanne",
+  "last_name": "Maldin",
+  "email": "emaldin2@hostgator.com",
+  "gender": "Male"
+}]`,
+  "gender-Female"
+);
