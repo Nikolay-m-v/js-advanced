17ef90d5374e2fe18e5f7b2bda722a56ee5af66f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-06-29 08:43:36 +0300

    task done

---
+++ Tasks Redo/construction-crew.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function constructionCrew(worker) {
+  if (worker.dizziness === true) {
+    worker.levelOfHydrated = +0.1 * worker.weight * worker.experience;
+    worker.dizziness = false;
+  }
+  console.log(worker);
+}
+
+constructionCrew({
+  weight: 80,
+  experience: 1,
+  levelOfHydrated: 0,
+  dizziness: true,
+});
