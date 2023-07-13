e5710138770ae2d9733c7abc3e0364a289edb3b0
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-13 12:00:32 +0300

    tasks

--- Additional tasks/OOP tasks/circle3.js
+++ Additional tasks/OOP tasks/circle3.js
@@ -1,7 +1,15 @@
 "use strict";
 function Circle(radius) {
   this.radius = radius;
+
+  let defaultLocation = { x: 0, y: 0 };
+
+  let computeOptimumLocation = function (factor) {};
+
   this.draw = function () {
+    this.computeOptimumLocation(0.1);
     console.log("Draw");
   };
 }
+
+const circle = new Circle(10);
