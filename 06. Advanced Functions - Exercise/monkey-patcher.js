ba854df3641a2ff7e4b1b918dc539e68ec7aafb2
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-16 11:49:02 +0300

    task done

--- 06. Advanced Functions - Exercise/monkey-patcher.js
+++ 06. Advanced Functions - Exercise/monkey-patcher.js
@@ -37,3 +37,15 @@ function result(command) {
 }
 
 result.call({ upvotes: 1, downvotes: 2 }, "upvote");
+let post = {
+  id: "3",
+  author: "emil",
+  content: "wazaaaaa",
+  upvotes: 100,
+  downvotes: 100,
+};
+solution.call(post, "upvote");
+solution.call(post, "downvote");
+let score = solution.call(post, "score"); // [127, 127, 0, 'controversial']
+solution.call(post, "downvote"); // (executed 50 times)
+score = solution.call(post, "score");
