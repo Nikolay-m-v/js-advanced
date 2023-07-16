4cc4b6dbd8812d247f21f9522f9f2648659bd2fb
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-16 11:40:39 +0300

    task done

---
+++ 06. Advanced Functions - Exercise/functional-sum.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function add(number) {
+  const inner = function (a) {
+    number += a;
+    return inner;
+  };
+
+  inner.toString = function () {
+    return number;
+  };
+
+  return inner;
+}
+
+console.log(add(1).toString());
+console.log(add(1)(6)(-3).toString());
