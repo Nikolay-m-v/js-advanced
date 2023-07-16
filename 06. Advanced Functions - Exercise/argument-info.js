478993b2a6ded135f2f6887cbe135981bb9a30e4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-16 10:11:16 +0300

    task done

--- 06. Advanced Functions - Exercise/argument-info.js
+++ 06. Advanced Functions - Exercise/argument-info.js
@@ -1,6 +1,23 @@
 "use strict";
 
-function argumentInfo() {}
+function argumentInfo(...params) {
+  let object = {};
+
+  for (let param of params) {
+    const type = typeof param;
+    console.log(`${type}: ${param}`);
+    if (!object.hasOwnProperty(type)) {
+      object[type] = 0;
+    }
+    object[type]++;
+  }
+
+  const sorted = Object.entries(object).sort((a, b) => b[1] - a[1]);
+
+  for (let [key, value] of sorted) {
+    console.log(`${key} = ${value}`);
+  }
+}
 
 argumentInfo("cat", 42, function () {
   console.log("Hello world!");
