1257a5df92f43bbc3cb23a3b85e0ade9dce2319d
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-07-17 10:51:56 +0300

    task done

---
+++ 07. Unit Testing and Error Handling - Lab/sub-sum.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function subSum(arr, startIndex, endIndex) {
+  let sum = 0;
+  if (Array.isArray(arr) === false) return NaN;
+  if (startIndex < 0) startIndex = 0;
+  if (endIndex > arr.length - 1) endIndex = arr.length - 1;
+
+  for (let i = startIndex; i <= endIndex; i++) {
+    sum += Number(arr[i]);
+  }
+  console.log(sum);
+}
+
+subSum([10, 20, 30, 40, 50, 60], 3, 300);
