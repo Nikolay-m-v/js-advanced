fde069b9da02d88d7ab8ddb6c92638331e5e99f9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-08-07 18:42:41 +0300

    task done

--- 08. Classes and Attributes - Lab/point-distance.js
+++ 08. Classes and Attributes - Lab/point-distance.js
@@ -8,17 +8,17 @@ class Point {
 
   static distance(pointOne, pointTwo) {
     const dx = pointOne.x - pointTwo.x;
-    const dy = pointTwo.y - pointTwo.y;
+    const dy = pointOne.y - pointTwo.y;
     return Math.sqrt(dx ** 2 + dy ** 2);
   }
 }
 
-const point1 = new Point(1, 1);
-const point2 = new Point(4, 5);
+// const point1 = new Point(1, 1);
+// const point2 = new Point(4, 5);
 
 let p1 = new Point(5, 5);
 let p2 = new Point(9, 8);
 console.log(Point.distance(p1, p2));
 
-console.log(point1, point2);
-console.log(Point.distance(point1, point2));
+// console.log(point1, point2);
+// console.log(Point.distance(point1, point2));
