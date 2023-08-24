983e44a69c087b48da0eb59e889f6940440aaaa2
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-08-24 12:58:58 +0300

    task done

---
+++ 09. Prototypes and Inheritance - Lab/extend-prototype.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function extendPrototype(classToExtend) {
+  classToExtend.prototype.species = "Human";
+  classToExtend.prototype.toSpeciesString = function () {
+    return `I am a ${this.species}. ${this.toString()}`;
+  };
+}
--- 09. Prototypes and Inheritance - Lab/inheriting-and-replacing-to-string.js
+++ 09. Prototypes and Inheritance - Lab/inheriting-and-replacing-to-string.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function personAndTeacher() {
+function inheritingAndReplacingToString() {
   return {
     Person,
     Teacher,
