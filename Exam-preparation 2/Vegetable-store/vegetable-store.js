cbda3eb9437fe96099fb6d1e827ef071eaa6c183
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-01 13:22:16 +0300

    working on vegetable store

--- Exam-preparation 2/Vegetable-store/vegetable-store.js
+++ Exam-preparation 2/Vegetable-store/vegetable-store.js
@@ -35,7 +35,34 @@ class VegetableStore {
     return `Sucessfully added ${uniqueTypes.join(", ")}`;
   }
 
-  buyingVegetables(selectedProducts) {}
+  buyingVegetables(selectedProducts) {
+    let totalPrice = 0;
+
+    for (let line of selectedProducts) {
+      let [type, quantity] = line.split(" ");
+      quantity = Number(quantity);
+      let product = this.availableProducts.find(
+        (vegetable) => (vegetable.type = type)
+      );
+      if (product === undefined) {
+        throw new Error(
+          `${type} is not available in the store, your current bil is ${totalPrice.toFixed(
+            2
+          )}`
+        );
+      }
+      if (quantity > product.quantity) {
+        throw new Error(
+          `The quantity ${quantity} for the vegetable ${type} is not available in the store, your current bill is ${totalPrice}`
+        );
+      }
+      totalPrice += product.price * quantity;
+      product.quantity -= quantity;
+    }
+    return `Great choice! You must pay the following amount ${totalPrice.toFixed(
+      2
+    )}`;
+  }
 }
 
 let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
