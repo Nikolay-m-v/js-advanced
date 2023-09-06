943c3ea4b15efb8785871f79fbcfc29099606e72
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-06 15:51:58 +0300

    additional task

--- Bars task/bars.js
+++ Bars task/bars.js
@@ -1,3 +1,29 @@
 "use strict";
 
-let personOne = {};
+let personOne = {
+  name: "William",
+  age: 28,
+  amountOfPullUps: 0,
+
+  pullUp(setOfPullUps) {
+    for (let i = 0; i < setOfPullUps.length; i++) {
+      if (setOfPullUps[i] === "chin above bar") {
+        this.amountOfPullUps++;
+        return `William has done a successfull pull up`;
+      } else {
+        return `William has failed to pull up his chin above the bar \nTotal amount of pull ups William did is: ${this.amountOfPullUps}`;
+        break;
+      }
+    }
+  },
+};
+
+personOne.pullUp([
+  "chin above bar",
+  "chin above bar",
+  "chin above bar",
+  "chin above bar",
+  "chin above bar",
+  "chin above bar",
+  "chin under bar",
+]);
