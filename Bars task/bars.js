3d4cfff4fc2b7fab00b9248e015ce662c5d636f7
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-06 16:10:12 +0300

    additional task

--- Bars task/bars.js
+++ Bars task/bars.js
@@ -4,21 +4,46 @@ let personOne = {
   name: "William",
   age: 28,
   amountOfPullUps: 0,
-
-  pullUp(setOfPullUps) {
-    for (let i = 0; i < setOfPullUps.length; i++) {
-      if (setOfPullUps[i] === "chin above bar") {
-        this.amountOfPullUps++;
-        return `William has done a successfull pull up`;
-      } else {
-        return `William has failed to pull up his chin above the bar \nTotal amount of pull ups William did is: ${this.amountOfPullUps}`;
-        break;
-      }
-    }
-  },
+  amountOfDips: 0,
 };
 
-personOne.pullUp([
+let personTwo = {
+  name: "Nick",
+  age: 29,
+  amountOfPullUps: 0,
+  amountOfDips: 0,
+};
+
+function pullUp(person, setOfPullUps) {
+  for (let i = 0; i < setOfPullUps.length; i++) {
+    let currentPullUp = setOfPullUps[i];
+    if (currentPullUp === "chin above bar") {
+      person.amountOfPullUps++;
+      console.log(`${person.name} has done a successfull pull up`);
+    } else {
+      console.log(
+        `${person.name} has failed to pull up his chin above the bar \nTotal amount of pull ups ${person.name} did is: ${person.amountOfPullUps}`
+      );
+      break;
+    }
+  }
+}
+
+function dips(person, setOfDips) {
+  for (let i = 0; i < setOfDips.length; i++) {
+    let currentDip = setOfDips[i];
+    if (currentDip === "90 degrees to almost extension") {
+      person.amountOfDips++;
+      console.log(`${person.name} has done a successfull dip`);
+    } else {
+      console.log(
+        `${person.name} has failed to a dip with full range of motion, therefore he fails. \nThe total amount of dips ${person.name} did is: ${person.amountOfDips} `
+      );
+    }
+  }
+}
+
+pullUp(personOne, [
   "chin above bar",
   "chin above bar",
   "chin above bar",
@@ -26,4 +51,22 @@ personOne.pullUp([
   "chin above bar",
   "chin above bar",
   "chin under bar",
+]);
+
+dips(personTwo, [
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "90 degrees to almost extension",
+  "unable to go full range of motion.",
 ]);
