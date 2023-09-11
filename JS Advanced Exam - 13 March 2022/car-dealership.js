ca8aadf234302603c89477505e175180f45abfd0
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-11 15:15:48 +0300

    exam tasks

--- JS Advanced Exam - 13 March 2022/car-dealership.js
+++ JS Advanced Exam - 13 March 2022/car-dealership.js
@@ -74,10 +74,10 @@ class CarDealership {
     ];
     this.soldCars.forEach((car) =>
       result.push(
-        `---${car.model} - ${car.horsePower} HP - ${car.price.toFixed(2)}`
+        `---${car.model} - ${car.horsepower} HP - ${car.soldPrice.toFixed(2)}$`
       )
     );
-    result.join("\n");
+    return result.join("\n");
   }
 }
 
@@ -86,9 +86,17 @@ class CarDealership {
 // console.log(dealership.addCar("Mercedes C63", 300, 29000, 187000));
 // console.log(dealership.addCar("", 120, 4900, 240000));
 
+// let dealership = new CarDealership("SoftAuto");
+// dealership.addCar("Toyota Corolla", 100, 3500, 190000);
+// dealership.addCar("Mercedes C63", 300, 29000, 187000);
+// dealership.addCar("Audi A3", 120, 4900, 240000);
+// console.log(dealership.sellCar("Toyota Corolla", 230000));
+// console.log(dealership.sellCar("Mercedes C63", 110000));
+
 let dealership = new CarDealership("SoftAuto");
 dealership.addCar("Toyota Corolla", 100, 3500, 190000);
 dealership.addCar("Mercedes C63", 300, 29000, 187000);
 dealership.addCar("Audi A3", 120, 4900, 240000);
-console.log(dealership.sellCar("Toyota Corolla", 230000));
-console.log(dealership.sellCar("Mercedes C63", 110000));
+dealership.sellCar("Toyota Corolla", 230000);
+dealership.sellCar("Mercedes C63", 110000);
+console.log(dealership.salesReport("horsepower"));
