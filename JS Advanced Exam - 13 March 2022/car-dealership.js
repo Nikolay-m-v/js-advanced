7bc74cd530f5f179ff855a913bcb38a8094c9ce6
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-11 15:14:36 +0300

    exam tasks

--- JS Advanced Exam - 13 March 2022/car-dealership.js
+++ JS Advanced Exam - 13 March 2022/car-dealership.js
@@ -81,7 +81,14 @@ class CarDealership {
   }
 }
 
+// let dealership = new CarDealership("SoftAuto");
+// console.log(dealership.addCar("Toyota Corolla", 100, 3500, 190000));
+// console.log(dealership.addCar("Mercedes C63", 300, 29000, 187000));
+// console.log(dealership.addCar("", 120, 4900, 240000));
+
 let dealership = new CarDealership("SoftAuto");
-console.log(dealership.addCar("Toyota Corolla", 100, 3500, 190000));
-console.log(dealership.addCar("Mercedes C63", 300, 29000, 187000));
-console.log(dealership.addCar("", 120, 4900, 240000));
+dealership.addCar("Toyota Corolla", 100, 3500, 190000);
+dealership.addCar("Mercedes C63", 300, 29000, 187000);
+dealership.addCar("Audi A3", 120, 4900, 240000);
+console.log(dealership.sellCar("Toyota Corolla", 230000));
+console.log(dealership.sellCar("Mercedes C63", 110000));
