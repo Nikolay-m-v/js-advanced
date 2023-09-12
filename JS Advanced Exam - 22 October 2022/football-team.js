800f691a51248e90b6bfc9fc147ab5253ce104a1
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-12 19:09:56 +0300

    fixed

--- JS Advanced Exam - 22 October 2022/football-team.js
+++ JS Advanced Exam - 22 October 2022/football-team.js
@@ -91,6 +91,31 @@ class footballTeam {
 //   ])
 // );
 
+// let fTeam = new footballTeam("Barcelona", "Spain");
+// console.log(
+//   fTeam.newAdditions([
+//     "Kylian Mbappé/23/160",
+//     "Lionel Messi/35/50",
+//     "Pau Torres/25/52",
+//   ])
+// );
+// console.log(fTeam.signContract("Lionel Messi/60"));
+// console.log(fTeam.signContract("Kylian Mbappé/240"));
+// console.log(fTeam.signContract("Barbukov/10"));
+
+// let fTeam = new footballTeam("Barcelona", "Spain");
+// console.log(
+//   fTeam.newAdditions([
+//     "Kylian Mbappé/23/160",
+//     "Lionel Messi/35/50",
+//     "Pau Torres/25/52",
+//   ])
+// );
+// console.log(fTeam.ageLimit("Lionel Messi", 33));
+// console.log(fTeam.ageLimit("Kylian Mbappé", 30));
+// console.log(fTeam.ageLimit("Pau Torres", 26));
+// console.log(fTeam.signContract("Kylian Mbappé/240"));
+
 let fTeam = new footballTeam("Barcelona", "Spain");
 console.log(
   fTeam.newAdditions([
@@ -99,6 +124,6 @@ console.log(
     "Pau Torres/25/52",
   ])
 );
-console.log(fTeam.signContract("Lionel Messi/60"));
 console.log(fTeam.signContract("Kylian Mbappé/240"));
-console.log(fTeam.signContract("Barbukov/10"));
+console.log(fTeam.ageLimit("Kylian Mbappé", 30));
+console.log(fTeam.transferWindowResult());
