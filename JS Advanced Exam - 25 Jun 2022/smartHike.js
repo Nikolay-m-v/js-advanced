c27c2f934abef53f142442b744575c675fb52f15
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-14 12:47:07 +0300

    new task from exams

--- JS Advanced Exam - 25 Jun 2022/smartHike.js
+++ JS Advanced Exam - 25 Jun 2022/smartHike.js
@@ -74,9 +74,26 @@ class SmartHike {
 // console.log(user.addGoal("Rui", 1706));
 // console.log(user.addGoal("Musala", 2925));
 
+// const user = new SmartHike("Vili");
+// console.log(user.addGoal("Musala", 2925));
+// console.log(user.addGoal("Rui", 1706));
+// console.log(user.hike("Musala", 8, "hard"));
+// console.log(user.hike("Rui", 3, "easy"));
+// console.log(user.hike("Everest", 12, "hard"));
+
+// const user = new SmartHike("Vili");
+// console.log(user.addGoal("Musala", 2925));
+// console.log(user.hike("Musala", 8, "hard"));
+// console.log(user.rest(4));
+// console.log(user.rest(5));
+
 const user = new SmartHike("Vili");
-console.log(user.addGoal("Musala", 2925));
-console.log(user.addGoal("Rui", 1706));
-console.log(user.hike("Musala", 8, "hard"));
-console.log(user.hike("Rui", 3, "easy"));
-console.log(user.hike("Everest", 12, "hard"));
+user.addGoal("Musala", 2925);
+user.hike("Musala", 8, "hard");
+console.log(user.showRecord("easy"));
+user.addGoal("Vihren", 2914);
+user.hike("Vihren", 4, "hard");
+console.log(user.showRecord("hard"));
+user.addGoal("Rui", 1706);
+user.hike("Rui", 3, "easy");
+console.log(user.showRecord("all"));
