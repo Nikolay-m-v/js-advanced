7df0b2ee157f0d77f58a851abd47bf4213444213
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-17 10:47:22 +0300

    exam task

---
+++ JS Advanced Final Exam - 23 October 2021/camping.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class SummerCamp {
+  constructor(organizer, location) {
+    this.organizer = organizer;
+    this.location = location;
+    this.priceForTheCamp = { child: 150, student: 300, collegian: 500 };
+    this.listOfParticipants = [];
+  }
+
+  registerParticipant(name, condition, money) {}
+}
