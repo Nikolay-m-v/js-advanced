fc59ec746b211e17a4451d9cac781ad4cece05b9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-18 15:56:13 +0300

    more tasks

--- JS Advanced Final Retake Exam - 10 December 2021/vegetable-store.js
+++ JS Advanced Final Retake Exam - 10 December 2021/vegetable-store.js
@@ -42,11 +42,27 @@ class VegetableStore {
       let [type, quantity] = line.split(" ");
       quantity = Number(quantity);
 
-      if (this.availableProducts.includes(type) === false) {
+      let product = this.availableProducts.find(
+        (vegetable) => vegetable.type === type
+      );
+      if (product === undefined) {
         throw new Error(
-          `${type} is not available in the store, your current bill is ${totalPrice}`
+          `${type} is not available in the store, your current bill is ${totalPrice.toFixed(
+            2
+          )}`
         );
       }
+
+      if (quantity > product.quantity) {
+        throw new Error(
+          `The quantity ${quantity} for the vegetable ${type} is not available in the store, your current bill is ${totalPrice}`
+        );
+      }
+      totalPrice += product.price * quantity;
+      product.quantity -= quantity;
     }
+    return `Great choice! You must pay the following amount $${totalPrice.toFixed(
+      2
+    )}`;
   }
 }
