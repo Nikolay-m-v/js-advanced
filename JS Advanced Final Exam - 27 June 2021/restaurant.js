30617c174f0e04f874a1cd5cb762f89ae15ca78f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-18 12:27:02 +0300

    more tasks

--- JS Advanced Final Exam - 27 June 2021/restaurant.js
+++ JS Advanced Final Exam - 27 June 2021/restaurant.js
@@ -94,7 +94,7 @@ class Restaurant {
   }
 }
 
-let kitchen = new Restaurant(1000);
+// let kitchen = new Restaurant(1000);
 // console.log(
 //   kitchen.loadProducts([
 //     "Banana 10 5",
@@ -106,26 +106,29 @@ let kitchen = new Restaurant(1000);
 //   ])
 // );
 
-console.log(
-  kitchen.addToMenu(
-    "frozenYogurt",
-    ["Yogurt 1", "Honey 1", "Banana 1", "Strawberries 10"],
-    9.99
-  )
-);
-console.log(
-  kitchen.addToMenu(
-    "Pizza",
-    [
-      "Flour 0.5",
-      "Oil 0.2",
-      "Yeast 0.5",
-      "Salt 0.1",
-      "Sugar 0.1",
-      "Tomato sauce 0.5",
-      "Pepperoni 1",
-      "Cheese 1.5",
-    ],
-    15.55
-  )
-);
+// console.log(
+//   kitchen.addToMenu(
+//     "frozenYogurt",
+//     ["Yogurt 1", "Honey 1", "Banana 1", "Strawberries 10"],
+//     9.99
+//   )
+// );
+// console.log(
+//   kitchen.addToMenu(
+//     "Pizza",
+//     [
+//       "Flour 0.5",
+//       "Oil 0.2",
+//       "Yeast 0.5",
+//       "Salt 0.1",
+//       "Sugar 0.1",
+//       "Tomato sauce 0.5",
+//       "Pepperoni 1",
+//       "Cheese 1.5",
+//     ],
+//     15.55
+//   )
+// );
+
+let kitchen = new Restaurant(1000);
+console.log(kitchen.showTheMenu());
