17e627cd845f72edc13c34e12bfe86b45720c270
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-18 16:10:27 +0300

    more tasks

--- JS Advanced Final Retake Exam - 10 December 2021/vegetable-store.js
+++ JS Advanced Final Retake Exam - 10 December 2021/vegetable-store.js
@@ -120,6 +120,19 @@ class VegetableStore {
 // console.log(vegStore.buyingVegetables(["Beans 8", "Okra 1.5"]));
 // console.log(vegStore.buyingVegetables(["Banana 1", "Beans 2"]));
 
+// let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
+// console.log(
+//   vegStore.loadingVegetables([
+//     "Okra 2.5 3.5",
+//     "Beans 10 2.8",
+//     "Celery 5.5 2.2",
+//     "Celery 0.5 2.5",
+//   ])
+// );
+// console.log(vegStore.rottingVegetable("Okra", 1));
+// console.log(vegStore.rottingVegetable("Okra", 2.5));
+// console.log(vegStore.buyingVegetables(["Beans 8", "Okra 1.5"]));
+
 let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
 console.log(
   vegStore.loadingVegetables([
@@ -131,4 +144,5 @@ console.log(
 );
 console.log(vegStore.rottingVegetable("Okra", 1));
 console.log(vegStore.rottingVegetable("Okra", 2.5));
-console.log(vegStore.buyingVegetables(["Beans 8", "Okra 1.5"]));
+console.log(vegStore.buyingVegetables(["Beans 8", "Celery 1.5"]));
+console.log(vegStore.revision());
