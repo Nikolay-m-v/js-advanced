6e55fd5524364b6970d296b0ecde705472c77aef
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-18 13:15:07 +0300

    more tasks

---
+++ JS Advanced Final Retake Exam - 10 December 2021/vegetable-store.js
@@ -0,0 +1,38 @@
+"use strict";
+
+class VegetableStore {
+  constructor(owner, location) {
+    this.owner = owner;
+    this.location = location;
+    this.availableProducts = [];
+  }
+
+  loadingVegetables(vegetables) {
+    let uniqueTypes = [];
+
+    for (let line of vegetables) {
+      let [type, quantity, price] = line.split(" ");
+      quantity = Number(quantity);
+      price = Number(price);
+
+      let product = this.availableProducts.find(
+        (vegetable) => vegetable.type === type
+      );
+      if (product === undefined) {
+        let vegetable = {
+          type,
+          quantity,
+          price,
+        };
+        this.availableProducts.push(vegetable);
+        uniqueTypes.push(type);
+      } else {
+        product.quantity += quantity;
+        if (product.price < price) {
+          product.price = price;
+        }
+      }
+    }
+    return `Successfully added ${uniqueTypes.join(", ")}`;
+  }
+}
