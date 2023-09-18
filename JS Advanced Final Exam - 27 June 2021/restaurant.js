b3fc9f67f4ba862f72a56e407c812f5882e98d6e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-18 12:25:27 +0300

    more tasks

--- JS Advanced Final Exam - 27 June 2021/restaurant.js
+++ JS Advanced Final Exam - 27 June 2021/restaurant.js
@@ -10,7 +10,7 @@ class Restaurant {
 
   loadProducts(products) {
     for (let line of products) {
-      const [productName, productQuantity, productTotalPrice] = line.split(" ");
+      let [productName, productQuantity, productTotalPrice] = line.split(" ");
       productQuantity = Number(productQuantity);
       productTotalPrice = Number(productTotalPrice);
       if (productTotalPrice <= this.budgetMoney) {
@@ -32,7 +32,66 @@ class Restaurant {
     return this.history.join("\n");
   }
 
-  addToMenu() {}
+  addToMenu(meal, neededProducts, price) {
+    let mealObj = { meal, product: [], price };
+
+    for (let line of neededProducts) {
+      let [productName, productQuantity] = line.split(" ");
+      productQuantity = Number(productQuantity);
+
+      let productObj = { productName, productQuantity };
+      mealObj.product.push(productObj);
+
+      if (this.menu.hasOwnProperty(mealObj.meal) === false) {
+        this.menu[meal] = mealObj;
+        if (Object.keys(this.menu).length === 1) {
+          return `Great idea! Now with the ${meal} we have 1 meal in the menu, other ideas?`;
+        } else {
+          return `Great idea! Now with the ${meal} we have ${
+            Object.keys(this.menu).length
+          } meals in the menu, other ideas?`;
+        }
+      } else
+        return `The ${meal} is already in our menu, try something different.`;
+    }
+  }
+
+  showTheMenu() {
+    if (Object.keys(this.menu).length === 0) {
+      return `Our menu is not ready yet, please come later...`;
+    } else {
+      let result = [];
+      Object.entries(this.menu).forEach((meal) =>
+        result.push(`${meal[0]} - ${meal[1].price}`)
+      );
+      return result.join("\n");
+    }
+  }
+
+  makeTheOrder(meal) {
+    let hasAllProducts = true;
+    if (Object.keys(this.menu).includes(meal) === false) {
+      return `There is not ${meal} yet in our menu, do you want to order something else?`;
+    } else {
+      for (let product of this.menu[meal].product) {
+        if (
+          this.stockProducts.hasOwnProperty(product.productName) === false ||
+          this.stockProducts[product.productName] < product.productQuantity
+        ) {
+          hasAllProducts = false;
+          break;
+        } else {
+          this.stockProducts[product.productName] -= product.productQuantity;
+          this.budgetMoney += this.menu[meal].price;
+        }
+      }
+    }
+    if (!hasAllProducts) {
+      return `For the time being, we cannot complete your order (${meal}), we are very sorry...`;
+    } else {
+      return `Your order (${meal}) will be completed in the next 30 minutes and will cost you ${this.menu[meal].price}`;
+    }
+  }
 }
 
 let kitchen = new Restaurant(1000);
