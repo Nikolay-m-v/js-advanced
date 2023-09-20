f077c849ef481430921f69c6c2120f35c0e35064
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-20 13:23:48 +0300

    exam task

--- JS Advanced Retake Exam - 10 Aug 2022/Triathlon.js
+++ JS Advanced Retake Exam - 10 Aug 2022/Triathlon.js
@@ -8,11 +8,46 @@ class Triathlon {
   }
 
   addParticipant(participantName, participantGender) {
-    if (this.participants.hasOwnProperty(participantName)) {
+    if (this.participants[participantName]) {
       return `${participantName} has already been added to the list.`;
     } else {
       this.participants[participantName] = participantGender;
       return `A new participant has been added - ${participantName}`;
+    }
+  }
+
+  completeness(participantName, condition) {
+    if (!this.participants[participantName]) {
+      throw new Error(
+        `${participantName} is not in the current participants list.`
+      );
+    }
+    if (this.participants[participantName] && condition < 30) {
+      throw new Error(
+        `${participantName} is not well prepared and cannot finish any discipline.`
+      );
+    }
+    let disciplinesCompleted = Math.floor(condition / 30);
+    if (disciplinesCompleted < 3) {
+      return `${participantName} could only complete ${disciplinesCompleted} of the disciplines.`;
+    } else {
+      this.listOfFinalists.push({
+        participantName,
+        participantGender: this.participants[participantName],
+      });
+      delete this.participants[participantName];
+      return `Congratulations, ${participantName} finished the whole competition`;
+    }
+  }
+
+  rewarding(participantName) {
+    const foundParticipant = this.listOfFinalists.find(
+      (participant) => participant.participantName === participantName
+    );
+    if (foundParticipant === undefined) {
+      return `${participantName} is not in the current finalists list.`;
+    } else {
+      return `${participantName} was rewarded with a trophy for his performance.`;
     }
   }
 }
