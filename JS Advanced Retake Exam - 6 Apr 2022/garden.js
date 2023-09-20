0ccc02f1b26db212e2feb7e56bf941e79e5c7b67
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-20 14:14:15 +0300

    exam task

--- JS Advanced Retake Exam - 6 Apr 2022/garden.js
+++ JS Advanced Retake Exam - 6 Apr 2022/garden.js
@@ -80,10 +80,18 @@ class Garden {
 // console.log(myGarden.addPlant("orange", 200));
 // console.log(myGarden.addPlant("olive", 50));
 
+// const myGarden = new Garden(250);
+// console.log(myGarden.addPlant("apple", 20));
+// console.log(myGarden.addPlant("orange", 100));
+// console.log(myGarden.addPlant("cucumber", 30));
+// console.log(myGarden.ripenPlant("apple", 10));
+// console.log(myGarden.ripenPlant("orange", 1));
+// console.log(myGarden.ripenPlant("orange", 4));
+
 const myGarden = new Garden(250);
 console.log(myGarden.addPlant("apple", 20));
 console.log(myGarden.addPlant("orange", 100));
 console.log(myGarden.addPlant("cucumber", 30));
 console.log(myGarden.ripenPlant("apple", 10));
 console.log(myGarden.ripenPlant("orange", 1));
-console.log(myGarden.ripenPlant("orange", 4));
+console.log(myGarden.ripenPlant("olive", 30));
