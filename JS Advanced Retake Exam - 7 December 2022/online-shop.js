cf3d30448703493ef976f964cac864febc1a9af3
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-23 13:07:12 +0300

    exam tasks

--- JS Advanced Retake Exam - 7 December 2022/online-shop.js
+++ JS Advanced Retake Exam - 7 December 2022/online-shop.js
@@ -15,4 +15,21 @@ class OnlineShop {
     this.warehouseSpace -= spaceRequired;
     return `The ${product} has been successfully delivered in the warehouse.`;
   }
+
+  quantityCheck(product, minimalQuantity) {
+    const foundProduct = this.products.find((p) => p.product === product);
+    if (foundProduct === undefined) {
+      throw new Error(`There is no ${product} in the warehouse.`);
+    }
+    if (minimalQuantity <= 0) {
+      throw new Error(`The quantity cannot be zero or negative.`);
+    }
+    if (minimalQuantity <= foundProduct.quantity) {
+      return `You have enough from product ${foundProduct}`;
+    }
+    foundProduct.quantity = minimalQuantity;
+    return `You added ${
+      foundProduct.quantity - minimalQuantity
+    } more from the ${minimalQuantity - foundProduct.quantity} products.`;
+  }
 }
