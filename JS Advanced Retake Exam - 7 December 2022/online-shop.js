233f017ba78820fed465dc7be9025773a599c32c
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-23 12:59:59 +0300

    exam tasks

---
+++ JS Advanced Retake Exam - 7 December 2022/online-shop.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class OnlineShop {
+  constructor(warehouseSpace) {
+    this.warehouseSpace = warehouseSpace;
+    this.products = [];
+    this.sales = [];
+  }
+
+  loadingStore(product, quantity, spaceRequired) {
+    if (spaceRequired > this.warehouseSpace) {
+      throw new Error(`Not enough space in the warehouse.`);
+    }
+  }
+}
