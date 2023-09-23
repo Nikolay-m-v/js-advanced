d1c7e34093b349abcd6b85c84bed4953f246ad85
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-23 13:16:58 +0300

    exam tasks

--- JS Advanced Retake Exam - 7 December 2022/online-shop.js
+++ JS Advanced Retake Exam - 7 December 2022/online-shop.js
@@ -60,3 +60,8 @@ class OnlineShop {
     );
   }
 }
+
+const myOnlineShop = new OnlineShop(500);
+console.log(myOnlineShop.loadingStore("headphones", 10, 200));
+console.log(myOnlineShop.loadingStore("laptop", 5, 200));
+console.log(myOnlineShop.loadingStore("TV", 40, 500));
