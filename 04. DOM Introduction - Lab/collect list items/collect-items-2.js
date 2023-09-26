2b4237405418581571407ba2a41065737af633aa
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-26 13:37:06 +0300

    learning DOM

---
+++ 04. DOM Introduction - Lab/collect list items/collect-items-2.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function collect() {
+  const items = Array.from(document.querySelectorAll("li"));
+  const result = items.map((el) => el.textContent).join("\n");
+  document.getElementById("result").value = result;
+}
