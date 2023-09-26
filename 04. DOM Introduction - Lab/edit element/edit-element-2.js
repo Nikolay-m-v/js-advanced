ea65b97e80bae8489daf84ee94d45d0be957134a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-26 13:32:38 +0300

    learning DOM

--- 04. DOM Introduction - Lab/edit element/edit-element-2.js
+++ 04. DOM Introduction - Lab/edit element/edit-element-2.js
@@ -2,6 +2,7 @@
 
 function editElement(element, match, replacer) {
   const text = element.textContent;
-  const result = text.split(match).join(replacer);
+  const pattern = new RegExp(match, "g");
+  const result = text.replace(pattern, replacer);
   element.textContent = result;
 }
