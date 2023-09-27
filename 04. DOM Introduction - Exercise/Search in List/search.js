04e68af2a22be50715fe04540a13d8a3ebf30de8
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-27 16:46:44 +0300

    dom

---
+++ 04. DOM Introduction - Exercise/Search in List/search.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function search() {
+  const search = document.getElementById("searchText").value;
+
+  const towns = document.querySelectorAll("ul#towns li");
+
+  for (const town of towns) {
+    town.style.fontWeight = "normal";
+    town.style.textDecoration = "";
+  }
+  let matches = 0;
+
+  for (const town of towns) {
+    if (town.textContent.includes(search)) {
+      matches++;
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+    }
+  }
+
+  document.getElementById("result").textContent = `${matches} matches found`;
+}
