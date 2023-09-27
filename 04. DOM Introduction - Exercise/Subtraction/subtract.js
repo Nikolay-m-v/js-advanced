fe131bdb55ba5df6e8bf130f53827fa906fdbdc3
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-27 15:25:07 +0300

    dom

--- 04. DOM Introduction - Exercise/Subtraction/subtract.js
+++ 04. DOM Introduction - Exercise/Subtraction/subtract.js
@@ -1,1 +1,8 @@
 "use strict";
+
+function subtract() {
+  let numberOne = Number(document.getElementById("firstNumber").value);
+  let numberTwo = Number(document.getElementById("secondNumber").value);
+
+  document.getElementById("result").textContent = numberOne - numberTwo;
+}
