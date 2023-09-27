1f44ffdc7fcc4c2985bc31ff565860a1f6748b85
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-09-27 15:45:20 +0300

    dom

--- 04. DOM Introduction - Exercise/Pascal or Camel case/solution.js
+++ 04. DOM Introduction - Exercise/Pascal or Camel case/solution.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function solve() {
+  let text = document
+    .getElementById("text")
+    .value.toLocaleLowerCase()
+    .split(" ");
+
+  let namingConvention = document.getElementById("naming-convention").value;
+
+  let output = "";
+  if (namingConvention === "Camel Case") {
+    output += text[0];
+
+    for (let i = 1; i < text.length; i++) {
+      output += text[i].charAt(0).toLocaleUpperCase() + text[i].substring(1);
+    }
+  }
+}
