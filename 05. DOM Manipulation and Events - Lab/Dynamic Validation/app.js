97b9ca4d8e6e0d34dbe05d0626ef675bf47a50ac
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-04 15:31:11 +0300

    dom tasks

--- 05. DOM Manipulation and Events - Lab/Dynamic Validation/app.js
+++ 05. DOM Manipulation and Events - Lab/Dynamic Validation/app.js
@@ -1,3 +1,15 @@
 "use strict";
 
-function validate() {}
+function validate() {
+  const email = document.getElementById("email");
+  email.addEventListener("change", onChange);
+
+  function onChange(event) {
+    const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/g;
+    if (pattern.test(event.target.value)) {
+      event.target.classList.remove("error");
+    } else {
+      event.target.classList.add("error");
+    }
+  }
+}
