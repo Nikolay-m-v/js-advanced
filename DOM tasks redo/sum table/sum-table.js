d8c3a55b00784b1969b26d3a5d965b14cfe1ecce
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-07 12:45:19 +0300

    redoing dom tasks to get better understanding

--- DOM tasks redo/sum table/sum-table.js
+++ DOM tasks redo/sum table/sum-table.js
@@ -2,10 +2,9 @@
 
 function sumTable() {
   const rows = Array.from(document.querySelectorAll("table tr"));
-
   let sum = 0;
 
-  for (let i = 1; i < rows.length - 1; i++) {
+  for (let i = 1; i < rows.length; i++) {
     sum += Number(rows[i].lastElementChild.textContent);
   }
 
