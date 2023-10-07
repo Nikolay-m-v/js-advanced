218cefb2740e1445b1105d8d7b1daa35ac128c20
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-07 12:16:08 +0300

    redoing dom tasks to get better understanding

--- DOM tasks redo/edit element/edit-element.js
+++ DOM tasks redo/edit element/edit-element.js
@@ -1,1 +1,8 @@
 "use strict";
+
+function editElement(element, match, replacer) {
+  const text = element.textContent;
+  const pattern = new RegExp(match, "g");
+  const result = text.replace(pattern, replacer);
+  element.textContent = result;
+}
