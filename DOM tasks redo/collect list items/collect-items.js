1ccd531d3333fd9a012d2a3d790552201c288940
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-07 12:21:35 +0300

    redoing dom tasks to get better understanding

--- DOM tasks redo/collect list items/collect-items.js
+++ DOM tasks redo/collect list items/collect-items.js
@@ -2,8 +2,6 @@
 
 function extractText() {
   const items = Array.from(document.querySelectorAll("li"));
-
-  const result = items.map((el) => el.textContent).join("\n");
-
+  const result = items.map((el) => el.textContent.join("\n"));
   document.getElementById("result").value = result;
 }
