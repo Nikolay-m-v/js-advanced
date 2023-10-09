ec00801a0adacc99ac7c00e529af1a89976e5111
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-09 13:15:11 +0300

    more dom practice

--- DOM tasks redo/Exercise Redo/Subtraction/subtract.js
+++ DOM tasks redo/Exercise Redo/Subtraction/subtract.js
@@ -1,1 +1,8 @@
 "use strict";
+
+function subtract() {
+  const num1 = Number(document.getElementById("firstNumber").value);
+  const num2 = Number(document.getElementById("secondNumber").value);
+
+  document.getElementById("result").textContent = num1 - num2;
+}
