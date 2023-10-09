d24954f8c9b181038d010f84b6b02b84b4b7717a
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-09 13:31:08 +0300

    more dom practice

--- DOM tasks redo/Exercise Redo/Pascal or Camel case/solution.js
+++ DOM tasks redo/Exercise Redo/Pascal or Camel case/solution.js
@@ -5,14 +5,12 @@ function solve() {
     .getElementById("text")
     .value.toLocaleLowerCase()
     .split(" ");
-
   let namingConvention = document.getElementById("naming-convention").value;
 
   let output = "";
 
   if (namingConvention === "Camel Case") {
     output += text[0];
-
     for (let i = 1; i < text.length; i++) {
       output += text[i].charAt(0).toLocaleUpperCase() + text[i].substring(1);
     }
@@ -21,8 +19,7 @@ function solve() {
       output += text[i].charAt(0).toLocaleUpperCase() + text[i].substring(1);
     }
   } else {
-    output + "Error!";
+    output = "Error!";
   }
-
   document.getElementById("result").textContent = output;
 }
