6b0459fcccc48bcf8b714769a58aeeea481e8e00
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-11 10:45:39 +0300

    more dom practice

--- DOM tasks redo/Exercise Redo/Search in List/search.js
+++ DOM tasks redo/Exercise Redo/Search in List/search.js
@@ -3,4 +3,21 @@
 function search() {
   const search = document.getElementById("searchText").value;
   const towns = document.querySelectorAll("ul#towns li");
+
+  for (const town of towns) {
+    town.style.fontWeight = "normal";
+    town.style.textDecoration = "";
+  }
+
+  let matches = 0;
+
+  for (const town of towns) {
+    if (town.textContent.includes(search) && search.length !== 0) {
+      matches++;
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+    }
+  }
+
+  document.getElementById("result").textContent = `${matches} matches found.`;
 }
