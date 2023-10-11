9d93eec291f4a91e4438427f9119b84bb0eaf274
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-11 12:12:33 +0300

    more dom practice

--- Bars task/app.js
+++ Bars task/app.js
@@ -1,3 +1,14 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("fighters div").addEventListener("click", onClick);
+  const fighters = document.querySelectorAll(".fighters div");
 
-function fight() {}
+  fighters.forEach((fighter) => {
+    fighter.addEventListener("click", function () {
+      const chosenFighter = this.cloneNode(true);
+      chosenFighter.classList.add("chosen");
+      document.querySelector(".chosenFighters").appendChild(chosenFighter);
+      this.remove();
+    });
+  });
+});
