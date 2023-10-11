2b59cbc58bcda3f52114806f222e13f320dfdf83
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-11 10:54:22 +0300

    more dom practice

--- DOM tasks redo/Exercise Redo/Table - Search Engine/app.js
+++ DOM tasks redo/Exercise Redo/Table - Search Engine/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function solve() {
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  let rows = document.querySelectorAll("tbody tr");
+  let input = document.getElementById("searchField");
+
+  function onClick() {
+    for (let row of rows) {
+      row.classList.remove("select");
+      if (row.innerHTML.includes(input.value) && input.value.length !== 0) {
+        row.className = "select";
+      }
+    }
+    input.value = "";
+  }
+}
