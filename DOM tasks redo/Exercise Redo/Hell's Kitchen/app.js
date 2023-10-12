56768c91b7ad29e209e1ea76622c69278bca300e
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-12 17:05:28 +0300

    dom practice

--- DOM tasks redo/Exercise Redo/Hell's Kitchen/app.js
+++ DOM tasks redo/Exercise Redo/Hell's Kitchen/app.js
@@ -4,6 +4,28 @@ function solve() {
   document.querySelector("#btnSend").addEventListener("click", onClick);
 
   function onClick() {
-    let inputArr = JSON.parse(document.querySelector("#input textarea").value);
+    let inputArr = JSON.parse(document.querySelector("#inputs textarea").value);
+    let averageSalary = 0;
+    let totalSalary = 0;
+    let currentAverageSalary = 0;
+    let bestRestaurant = "";
+    let output = {};
+
+    for (let inputElement of inputArr) {
+      let restaurantInfo = inputElement.split(" - ");
+      let restaurantName = restaurantInfo.shift();
+      let workersData = restaurantInfo[0].split(", ");
+
+      for (let worker of workersData) {
+        let [name, salary] = worker.split(" ");
+        if (!output.hasOwnProperty(restaurantName)) {
+          output[restaurantName] = {};
+        }
+
+        if (output.hasOwnProperty(restaurantName)) {
+          output[restaurantName][name] = Number(salary);
+        }
+      }
+    }
   }
 }
