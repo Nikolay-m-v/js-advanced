1857ab35a9f8b4ba1084b825d05cfa5fcb919023
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-15 10:17:43 +0300

    dom tasks

--- DOM tasks redo/05. DOM Manipulation and Events - Lab/Mouse Gradient/app.js
+++ DOM tasks redo/05. DOM Manipulation and Events - Lab/Mouse Gradient/app.js
@@ -1,31 +1,12 @@
 "use strict";
 
-// function attachGradientEvents() {
-//   const gradient = document.getElementById("gradient");
-//   gradient.addEventListener("mousemove", onMouseOver);
-//   const result = document.getElementById("result");
-
-//   function onMouseOver(event) {
-//     result.textContent =
-//       Math.floor((event.offsetX / gradient.clientWidth) * 100) * "%";
-//   }
-// }
-
 function attachGradientEvents() {
-  const inputFields = document.querySelectorAll("input");
+  const gradient = document.getElementById("gradient");
+  gradient.addEventListener("mousemove", onMouseOver);
+  const result = document.getElementById("result");
 
-  inputFields.forEach((input) => {
-    input.addEventListener("focus", highlightSection);
-    input.addEventListener("blur", removeHighlight);
-  });
-
-  function highlightSection(event) {
-    const parentDiv = event.target.parentElement;
-    parentDiv.classList.add("focused");
-  }
-
-  function removeHighlight(event) {
-    const parentDiv = event.target.parentElement;
-    parentDiv.classList.remove("focused");
+  function onMouseOver() {
+    result.textContent =
+      Math.floor((event.offsetX / gradient.clientWidth) * 100) + "%";
   }
 }
