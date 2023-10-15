3127a04a834023a0b87beecf317732050ed2f1b9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-15 10:13:13 +0300

    dom tasks

--- DOM tasks redo/05. DOM Manipulation and Events - Lab/Add or Delete/app.js
+++ DOM tasks redo/05. DOM Manipulation and Events - Lab/Add or Delete/app.js
@@ -1,3 +1,21 @@
 "use strict";
 
-function addItem() {}
+function addItem() {
+  const input = document.getElementById("newItemText").value;
+  const li = document.createElement("li");
+  li.textContent = input;
+  if (li.textContent.length !== 0) {
+    document.getElementById("items").appendChild(li);
+    document.getElementById("newItemText").value = "";
+  }
+
+  const deleteButton = document.createElement("a");
+  deleteButton.textContent = `[Delete]`;
+  deleteButton.href = "#";
+  li.appendChild(deleteButton);
+  deleteButton.addEventListener("click", onDelete);
+
+  function onDelete(event) {
+    event.target.parentElement.remove();
+  }
+}
