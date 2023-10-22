3815914909524ee5b943573c6be2f2dde7740ac8
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-22 12:37:43 +0300

    more dom practice

--- JS Advanced Exam - 13 March 2022/Mails Delivery/app.js
+++ JS Advanced Exam - 13 March 2022/Mails Delivery/app.js
@@ -1,13 +1,23 @@
 "use strict";
 
-function solution() {
+function solve() {
   const inputs = {
     recipientName: document.getElementById("recipientName"),
     title: document.getElementById("title"),
     message: document.getElementById("message"),
   };
 
-  const [addBtn, clearBtn] = Array.from(document.querySelectorAll("button"));
+  const addBtn = document.getElementById("add");
+  const resetBtn = document.getElementById("reset");
+
+  resetBtn.addEventListener("click", reset);
+
+  function reset(event) {
+    event.preventDefault();
+    inputs.recipientName.value = "";
+    inputs.title.value = "";
+    inputs.message.value = "";
+  }
 
   addBtn.addEventListener("click", add);
 
@@ -22,11 +32,52 @@ function solution() {
       return;
     }
 
-    let li = document.createElement("li");
-    let h4 = document.createElement("h4");
+    const ul = document.getElementById("list");
+
+    const li = document.createElement("li");
+
+    let titleH4 = document.createElement("h4");
+    titleH4.textContent = `Title: ${title}`;
+
+    let nameH4 = document.createElement("h4");
+    nameH4.textContent = `Recipient Name: ${recipientName}`;
+
     let span = document.createElement("span");
+    span.textContent = message;
+
     let div = document.createElement("div");
+    div.id = "list-action";
+
     let sendBtn = document.createElement("button");
+    sendBtn.type = "submit";
+    sendBtn.id = "send";
+    sendBtn.textContent = "Send";
+
     let deleteBtn = document.createElement("button");
+    deleteBtn.type = "submit";
+    deleteBtn.id = "delete";
+    deleteBtn.textContent = "Delete";
+
+    ul.appendChild(li);
+    li.appendChild(titleH4);
+    li.appendChild(nameH4);
+    li.appendChild(span);
+    li.appendChild(div);
+    div.appendChild(sendBtn);
+    div.appendChild(deleteBtn);
+
+    inputs.recipientName.value = "";
+    inputs.title.value = "";
+    inputs.message.value = "";
+
+    sendBtn.addEventListener("click", send);
+
+    function send() {
+      li.remove();
+
+      const ulSent = document.querySelector(".sent-list");
+
+      const liSent = document.createAttribute("li");
+    }
   }
 }
