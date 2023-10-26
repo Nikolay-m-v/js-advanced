aa76d368396beed794d23b8ec0ddb30784ff241c
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-26 14:54:25 +0300

    added just a small side project

--- Bars task/app.js
+++ Bars task/app.js
@@ -1,14 +1,14 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("fighters div").addEventListener("click", onClick);
-  const fighters = document.querySelectorAll(".fighters div");
 
-  fighters.forEach((fighter) => {
-    fighter.addEventListener("click", function () {
-      const chosenFighter = this.cloneNode(true);
-      chosenFighter.classList.add("chosen");
-      document.querySelector(".chosenFighters").appendChild(chosenFighter);
-      this.remove();
-    });
+function chooseFighter() {
+  const fighterOne = document.querySelector(".fighter-one");
+  const fighterTwo = document.querySelector(".fighter-two");
+  const fighterThree = document.querySelector(".fighter-three");
+  const chosenFighterOne = document.querySelector(".chosenFighterOne");
+
+  fighterOne.addEventListener("click", () => {
+    chosenFighterOne.appendChild(fighterOne);
   });
-});
+}
+
+chooseFighter();
