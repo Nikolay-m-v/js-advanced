3b2a5580897fd6b01a71c1a98e35f15c72436fd4
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-26 16:51:08 +0300

    added just a small side project

--- Bars task/app.js
+++ Bars task/app.js
@@ -1,10 +1,6 @@
 "use strict";
 
 function chooseFighter() {
-  const fightersArray = Array.from(document.querySelectorAll(".fighter"));
-  const chosenFighters = document.querySelector(".chosenFighters");
-  const fighters = document.querySelector(".fighters");
-
   fightersArray.forEach((fighter) => {
     fighter.addEventListener("click", () => {
       if (
@@ -23,4 +19,48 @@ function chooseFighter() {
   });
 }
 
+function toTheArena(chosenFighters, arenaBattleImage, arenaImg) {
+  arenaImg.addEventListener("click", () => {
+    if (chosenFighters.children.length >= 2) {
+      alert("Entering the arena!");
+      arenaBattleImage.classList.add("inside-the-arena");
+      arenaImg.style.display = "none";
+      Array.from(chosenFighters.children).forEach((child) => {
+        child.classList.add("face-img-arena");
+      });
+    }
+  });
+}
+
+function resetBtnFunction(chosenFighters) {
+  chosenFighters.innerHTML = "";
+  arenaBattleImage.classList.remove("inside-the-arena");
+  arenaImg.style.display = "block";
+}
+
+function eventHandling(elements) {
+  elements.resetBtn.addEventListener("click", () => {
+    resetBtnFunction(elements.chosenFighters);
+  });
+}
+
+function elementGathering() {
+  const fightersArray = Array.from(document.querySelectorAll(".fighter"));
+  const chosenFighters = document.querySelector(".chosenFighters");
+  const fighters = document.querySelector(".fighters");
+  const arenaImg = document.querySelector(".arena-img");
+  const arenaBattleImage = document.querySelector(".arena-battle-img");
+  const resetBtn = document.getElementById("resetBtn");
+
+  return {
+    fightersArray,
+    chosenFighters,
+    fighters,
+    arenaImg,
+    arenaBattleImage,
+    resetBtn,
+  };
+}
+
 chooseFighter();
+toTheArena();
