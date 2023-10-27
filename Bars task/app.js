7a58787bd7f64f6e0eb311dcc59e33800db696f9
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-10-27 14:25:50 +0300

    working on side project dom practice

--- Bars task/app.js
+++ Bars task/app.js
@@ -1,66 +1,71 @@
 "use strict";
+(function main() {
+  function chooseFighter(elements, fighter) {
+    if (
+      elements.chosenFighters.children.length >= 2 &&
+      !fighter.parentElement.classList.contains("chosenFighters")
+    ) {
+      return;
+    }
 
-function chooseFighter() {
-  fightersArray.forEach((fighter) => {
-    fighter.addEventListener("click", () => {
-      if (
-        chosenFighters.children.length >= 2 &&
-        !fighter.parentElement.classList.contains("chosenFighters")
-      ) {
-        return;
-      }
+    if (fighter.parentElement === elements.chosenFighters) {
+      elements.fighters.appendChild(fighter);
+    } else {
+      elements.chosenFighters.appendChild(fighter);
+    }
+  }
 
-      if (fighter.parentElement === chosenFighters) {
-        fighters.appendChild(fighter);
-      } else {
-        chosenFighters.appendChild(fighter);
-      }
-    });
-  });
-}
-
-function toTheArena(chosenFighters, arenaBattleImage, arenaImg) {
-  arenaImg.addEventListener("click", () => {
-    if (chosenFighters.children.length >= 2) {
+  function toTheArena(chosenFighters, arenaBattleImage, arenaImg) {
+    if (elements.chosenFighters.children.length >= 2) {
       alert("Entering the arena!");
-      arenaBattleImage.classList.add("inside-the-arena");
-      arenaImg.style.display = "none";
-      Array.from(chosenFighters.children).forEach((child) => {
+      elements.arenaBattleImage.classList.add("inside-the-arena");
+      elements.arenaImg.style.display = "none";
+      Array.from(elements.chosenFighters.children).forEach((child) => {
         child.classList.add("face-img-arena");
       });
     }
-  });
-}
+  }
 
-function resetBtnFunction(chosenFighters) {
-  chosenFighters.innerHTML = "";
-  arenaBattleImage.classList.remove("inside-the-arena");
-  arenaImg.style.display = "block";
-}
+  function resetBtnFunction(elements) {
+    elements.chosenFighters.innerHTML = "";
+    elements.arenaBattleImage.classList.remove("inside-the-arena");
+    elements.arenaImg.style.display = "block";
+  }
 
-function eventHandling(elements) {
-  elements.resetBtn.addEventListener("click", () => {
-    resetBtnFunction(elements.chosenFighters);
-  });
-}
+  function eventHandling(elements) {
+    elements.resetBtn.addEventListener("click", () => {
+      resetBtnFunction(elements);
+    });
 
-function elementGathering() {
-  const fightersArray = Array.from(document.querySelectorAll(".fighter"));
-  const chosenFighters = document.querySelector(".chosenFighters");
-  const fighters = document.querySelector(".fighters");
-  const arenaImg = document.querySelector(".arena-img");
-  const arenaBattleImage = document.querySelector(".arena-battle-img");
-  const resetBtn = document.getElementById("resetBtn");
+    elements.fightersArray.forEach((fighter) => {
+      fighter.addEventListener("click", chooseFighter(elements, fighter));
+    });
 
-  return {
-    fightersArray,
-    chosenFighters,
-    fighters,
-    arenaImg,
-    arenaBattleImage,
-    resetBtn,
-  };
-}
+    elements.arenaImg.addEventListener("click", () => {
+      toTheArena(
+        elements.chosenFighters,
+        elements.arenaBattleImage,
+        elements.arenaImg
+      );
+    });
+  }
 
-chooseFighter();
-toTheArena();
+  function elementGathering() {
+    const fightersArray = Array.from(document.querySelectorAll(".fighter"));
+    const chosenFighters = document.querySelector(".chosenFighters");
+    const fighters = document.querySelector(".fighters");
+    const arenaImg = document.querySelector(".arena-img");
+    const arenaBattleImage = document.querySelector(".arena-battle-img");
+    const resetBtn = document.getElementById("resetBtn");
+
+    return {
+      fightersArray,
+      chosenFighters,
+      fighters,
+      arenaImg,
+      arenaBattleImage,
+      resetBtn,
+    };
+  }
+  eventHandling(elementGathering());
+})();
