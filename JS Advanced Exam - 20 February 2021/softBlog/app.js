b37513381b3db56d0f022bba4a359999b2ec3025
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-02 14:40:27 +0200

    dom practice

--- JS Advanced Exam - 20 February 2021/softBlog/app.js
+++ JS Advanced Exam - 20 February 2021/softBlog/app.js
@@ -5,12 +5,12 @@ function solve() {
   const inputTitle = document.getElementById("title");
   const inputCategory = document.getElementById("category");
   const inputContent = document.getElementById("content");
-  const articleSection = document.querySelector("section");
+  const articleSection = document.querySelectorAll("section")[1];
 
   createBtn.addEventListener("click", create);
 
   function create(event) {
-    event.preventDefault;
+    event.preventDefault();
 
     const creator = inputCreator.value;
     const title = inputTitle.value;
@@ -22,8 +22,8 @@ function solve() {
     const h1Title = document.createElement("h1");
     h1Title.textContent = title;
 
-    const pElement = document.createElement("p");
-    pElement.textContent = `Category:`;
+    const pCategory = document.createElement("p");
+    pCategory.textContent = `Category:`;
 
     const strongCategory = document.createElement("strong");
     strongCategory.textContent = category;
@@ -35,9 +35,49 @@ function solve() {
     strongCreator.textContent = creator;
 
     const pContent = document.createElement("p");
-    pContent.textContent = "lorem*10  ";
+    pContent.textContent = content;
 
+    const divButtons = document.createElement("div");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
+    const archiveBtn = document.createElement("button");
+    archiveBtn.textContent = "archive";
+
+    divButtons.classList.add("buttons");
+    deleteBtn.classList.add("btn", "delete");
+    archiveBtn.classList.add("btn", "archive");
+    divButtons.appendChild(deleteBtn);
+    divButtons.appendChild(archiveBtn);
+
+    archiveBtn.addEventListener("click", archive);
+    function archive() {
+      articleElement.remove();
+
+      const olList = document.querySelector("ol");
+
+      const liArchive = document.createElement("li");
+      liArchive.textContent = title;
+      olList.appendChild(liArchive);
+
+      const titles = Array.from(document.querySelectorAll("li"));
+      titles
+        .sort((a, b) => a.textContent.localeCompare(b.textContent))
+        .forEach((node) => olList.appendChild(node));
+    }
+
+    deleteBtn.addEventListener("click", deleteFunction);
+
+    function deleteFunction() {
+      articleElement.remove();
+    }
+
+    articleElement.appendChild(h1Title);
+    articleElement.appendChild(pCategory);
+    articleElement.appendChild(strongCategory);
+    articleElement.appendChild(pCreator);
+    articleElement.appendChild(strongCreator);
+    articleElement.appendChild(pContent);
+    articleElement.appendChild(divButtons);
     articleSection.appendChild(articleElement);
-    articleSection.appendChild(h1Title);
   }
 }
