4a9c2454cda9f1b4534eee2db44af519dc499e5f
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-03 17:36:48 +0200

    rock paper scissors game

---
+++ Additional tasks/Rock Paper Scissors game/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function rockPaperScissorsGame() {
+  function getElements() {
+    const playButton = document.getElementById("play");
+    const titleAnnouncer = document.getElementsByClassName("announcer");
+  }
+}
