e718a91d69470ee8ef998edc7cdffdd2d62a7a02
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-03 20:30:07 +0200

    rock paper scissors game

--- Additional tasks/Rock Paper Scissors game/app.js
+++ Additional tasks/Rock Paper Scissors game/app.js
@@ -3,6 +3,12 @@
   let playerScore = 0;
   let computerScore = 0;
   let computerGeneratedChoice = "";
+  let playerWin = false;
+  let computerWin = false;
+  let isDraw = false;
+
+  const pScore = document.getElementsByClassName("player-points");
+  const cScore = document.getElementsByClassName("computer-points");
 
   function playButtonFunction(elements) {
     elements.imagesArray.forEach((img) => {
@@ -17,49 +23,79 @@
   }
 
   function rockButtonFunction(elements) {
+    elements.chosenPlayerImage[0].src = "assets/rock.png";
     generateComputerChoice(elements);
     let playerChoice = "rock";
     if (playerChoice === computerGeneratedChoice) {
       elements.titleAnnouncer[0].textContent = "Draw!";
+      isDraw = true;
     } else if (computerGeneratedChoice === "paper") {
       elements.titleAnnouncer[0].textContent = "Computer Wins!";
+      computerWin = true;
     } else {
       elements.titleAnnouncer[0].textContent = "Player Wins!";
+      playerWin = true;
     }
   }
 
   function paperButtonFunction(elements) {
     generateComputerChoice(elements);
+    elements.chosenPlayerImage[0].src = "assets/paper.png";
     let playerChoice = "paper";
     if (playerChoice === computerGeneratedChoice) {
       elements.titleAnnouncer[0].textContent = "Draw!";
+      isDraw = true;
     } else if (computerGeneratedChoice === "rock") {
       elements.titleAnnouncer[0].textContent = "Player Wins!";
+      playerWin = true;
     } else {
       elements.titleAnnouncer[0].textContent = "Computer Wins!";
+      computerWin = true;
     }
   }
 
   function scissorsButtonFunction(elements) {
+    elements.chosenPlayerImage[0].src = "assets/scissors.png";
     generateComputerChoice(elements);
     let playerChoice = "scissors";
     if (playerChoice === computerGeneratedChoice) {
       elements.titleAnnouncer[0].textContent = "Draw!";
+      isDraw = true;
     } else if (computerGeneratedChoice === "rock") {
       elements.titleAnnouncer[0].textContent = "Computer Wins!";
+      computerWin = true;
     } else {
       elements.titleAnnouncer[0].textContent = "Player Wins!";
+      playerWin = true;
     }
+  }
+
+  function scoreUpdate() {
+    if (playerWin) {
+      playerScore++;
+      pScore[0].textContent = playerScore;
+    } else if (computerWin) {
+      computerScore++;
+      cScore[0].textContent = computerScore;
+    } else if (isDraw) {
+      return;
+    }
+    playerWin = false;
+    computerWin = false;
+    isDraw = false;
   }
 
   function generateComputerChoice(elements) {
     let numberDefiningChoice = Math.floor(Math.random() * 30) + 1;
     if (numberDefiningChoice <= 10) {
       computerGeneratedChoice = "rock";
+      elements.computerImage[0].src = "assets/rock.png";
     } else if (numberDefiningChoice > 10 && numberDefiningChoice <= 20) {
       computerGeneratedChoice = "paper";
+      elements.computerImage[0].src = "assets/paper.png";
     } else {
       computerGeneratedChoice = "scissors";
+      elements.computerImage[0].src = "assets/scissors.png";
     }
     return computerGeneratedChoice;
   }
@@ -71,14 +107,17 @@
 
     elements.rockButton[0].addEventListener("click", () => {
       rockButtonFunction(elements);
+      scoreUpdate(playerWin);
     });
 
     elements.paperButton[0].addEventListener("click", () => {
       paperButtonFunction(elements);
+      scoreUpdate(playerWin);
     });
 
     elements.scissorsButton[0].addEventListener("click", () => {
       scissorsButtonFunction(elements);
+      scoreUpdate(playerWin);
     });
   }
 
@@ -90,10 +129,14 @@
     const scissorsButton = document.getElementsByClassName("scissorsChoice");
     const imagesArray = Array.from(document.querySelectorAll(".hidden-img"));
     const images = document.getElementsByClassName("images");
-    const pScore = document.getElementsByClassName("player-points");
-    const cScore = document.getElementsByClassName("computer-points");
+    // const pScore = document.getElementsByClassName("player-points");
+    // const cScore = document.getElementsByClassName("computer-points");
     const computerStartingImage = document.getElementsByClassName(
       "computer-starting-image"
+    );
+    const chosenPlayerImage = document.getElementsByClassName("chosenByPlayer");
+    const computerImage = document.getElementsByClassName(
+      "computerChosenImage"
     );
 
     return {
@@ -104,9 +147,11 @@
       scissorsButton,
       imagesArray,
       images,
-      pScore,
-      cScore,
+      // pScore,
+      // cScore,
       computerStartingImage,
+      chosenPlayerImage,
+      computerImage,
     };
   }
 
