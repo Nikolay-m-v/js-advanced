f113002732f5aae442e31e66121ef1503a3d9a09
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-03 18:51:26 +0200

    rock paper scissors game

--- Additional tasks/Rock Paper Scissors game/app.js
+++ Additional tasks/Rock Paper Scissors game/app.js
@@ -1,8 +1,41 @@
 "use strict";
+(function main() {
+  function playButtonFunction(elements) {
+    elements.imagesArray.forEach((img) => {
+      img.classList.remove("hidden-img");
+    });
+    elements.rockButton[0].style.display = "block";
+    elements.paperButton[0].style.display = "block";
+    elements.scissorsButton[0].style.display = "block";
+    elements.playButton.style.display = "none";
+    elements.titleAnnouncer[0].textContent = "Choose an option";
+  }
 
-function rockPaperScissorsGame() {
+  function eventHandling(elements) {
+    elements.playButton.addEventListener("click", () => {
+      playButtonFunction(elements);
+    });
+  }
+
   function getElements() {
     const playButton = document.getElementById("play");
     const titleAnnouncer = document.getElementsByClassName("announcer");
+    const rockButton = document.getElementsByClassName("rockChoice");
+    const paperButton = document.getElementsByClassName("paperChoice");
+    const scissorsButton = document.getElementsByClassName("scissorsChoice");
+    const imagesArray = Array.from(document.querySelectorAll(".hidden-img"));
+    const images = document.getElementsByClassName("images");
+
+    return {
+      playButton,
+      titleAnnouncer,
+      rockButton,
+      paperButton,
+      scissorsButton,
+      imagesArray,
+      images,
+    };
   }
-}
+
+  eventHandling(getElements());
+})();
