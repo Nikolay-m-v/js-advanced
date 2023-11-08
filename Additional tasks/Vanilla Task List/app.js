06563378c4db3c76d6585876a645ce211f8906af
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-08 18:33:08 +0200

    dom practice

--- Additional tasks/Vanilla Task List/app.js
+++ Additional tasks/Vanilla Task List/app.js
@@ -8,17 +8,27 @@
     const completeButton = document.createElement("button");
     const deleteButton = document.createElement("button");
     const buttonsHolder = document.createElement("div");
+
     completeButton.classList.add("complete-btn");
+    completeButton.id = "completeBtn";
     completeButton.innerHTML = `<i class="fas fa-check"></i>`;
     deleteButton.classList.add("delete-btn");
+    deleteButton.id = "deleteBtn";
     deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
     li.textContent = inputText.value;
     li.classList.add("all-list-styling");
+
     elements.allListItems.appendChild(li);
     li.appendChild(buttonsHolder);
     buttonsHolder.appendChild(completeButton);
     buttonsHolder.appendChild(deleteButton);
+    deleteButton.addEventListener("click", () => {
+      elements.deletedListItems.appendChild(li);
+    });
+    completeButton.addEventListener("click", () => {});
   }
+
+  function deleteTask(elements) {}
 
   function eventHandling(elements) {
     elements.submitButton.addEventListener("click", (event) => {
@@ -32,18 +42,17 @@
     const submitButton = document.getElementById("submitButton");
     const allListItems = document.getElementById("allListItems");
     const completedListItems = document.getElementById("completed");
-    const notCompletedListItems = document.getElementById(
-      "allLinotCompletedstItems"
-    );
+    const deletedListItems = document.getElementById("deletedListItems");
 
     return {
       inputText,
       submitButton,
       allListItems,
       completedListItems,
-      notCompletedListItems,
+      deletedListItems,
     };
   }
 
+  eventHandling(getElements());
   eventHandling(getElements());
 })();
