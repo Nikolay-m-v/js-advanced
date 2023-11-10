6bde65dd9624c9fdf3b8002ba241074b6ac6a8a1
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-11 00:48:40 +0200

    more dom tasks

--- JS Advanced Exam - 22 October 2022/Scary Story/app.js
+++ JS Advanced Exam - 22 October 2022/Scary Story/app.js
@@ -1,1 +1,107 @@
 "use strict";
+
+(function main() {
+  function createElements(elements) {
+    const liEl = document.createElement("li");
+    liEl.classList.add("story-info");
+
+    const articleEl = document.createElement("article");
+
+    const h4El = document.createElement("h4");
+    h4El.textContent = `Name: ${elements.firstNameInput.value} ${elements.lastNameInput.value}`;
+
+    const pAge = document.createElement("p");
+    pAge.textContent = `Age : ${elements.ageInput.value}`;
+
+    const pTitle = document.createElement("p");
+    pTitle.textContent = `Title: ${elements.storyTitleInput.value}`;
+
+    const pGenre = document.createElement("p");
+    pGenre.textContent = `Genre: ${elements.genreSelector.value}`;
+
+    const pStory = document.createElement("p");
+    pStory.textContent = `${elements.storyInput.value}`;
+
+    const saveBtn = document.createElement("button");
+    saveBtn.textContent = "Save story";
+    saveBtn.classList.add("save-btn");
+
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit story";
+    editBtn.classList.add("edit-btn");
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete story";
+    deleteBtn.classList.add("delete-btn");
+
+    appendElements(elements, {
+      liEl,
+      articleEl,
+      h4El,
+      pAge,
+      pTitle,
+      pGenre,
+      pStory,
+      saveBtn,
+      editBtn,
+      deleteBtn,
+    });
+
+    return {
+      liEl,
+      articleEl,
+      h4El,
+      pAge,
+      pTitle,
+      pGenre,
+      pStory,
+      saveBtn,
+      editBtn,
+      deleteBtn,
+    };
+  }
+
+  function appendElements(elements, elementsToAppend) {
+    elements.previewStory.appendChild(elementsToAppend.liEl);
+    elements.previewStory.appendChild(elementsToAppend.articleEl);
+    elementsToAppend.articleEl.appendChild(elementsToAppend.h4El);
+    elementsToAppend.articleEl.appendChild(elementsToAppend.pAge);
+    elementsToAppend.articleEl.appendChild(elementsToAppend.pTitle);
+    elementsToAppend.articleEl.appendChild(elementsToAppend.pStory);
+    elements.previewStory.appendChild(elementsToAppend.saveBtn);
+    elements.previewStory.appendChild(elementsToAppend.editBtn);
+    elements.previewStory.appendChild(elementsToAppend.deleteBtn);
+  }
+
+  function publishStory(elements) {}
+
+  function eventHandling(elements) {
+    elements.publishBtn.addEventListener("click", () => {
+      createElements(elements);
+    });
+  }
+
+  function getElements() {
+    const firstNameInput = document.getElementById("first-name");
+    const lastNameInput = document.getElementById("last-name");
+    const ageInput = document.getElementById("age");
+    const storyTitleInput = document.getElementById("story-title");
+    const publishBtn = document.getElementById("form-btn");
+    const previewStory = document.getElementById("preview-list");
+    const genreSelector = document.getElementById("genre");
+    const storyInput = document.getElementById("story");
+
+    return {
+      firstNameInput,
+      lastNameInput,
+      ageInput,
+      storyTitleInput,
+      publishBtn,
+      previewStory,
+      genreSelector,
+      storyInput,
+    };
+  }
+
+  eventHandling(getElements());
+})();
