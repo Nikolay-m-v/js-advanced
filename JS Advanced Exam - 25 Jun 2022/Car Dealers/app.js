8aae11794ab0d1d61f6c60c11c0a09cb871f4b90
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2023-11-14 12:21:38 +0200

    dom practice

--- JS Advanced Exam - 25 Jun 2022/Car Dealers/app.js
+++ JS Advanced Exam - 25 Jun 2022/Car Dealers/app.js
@@ -1,6 +1,43 @@
 "use strict";
 
 (function main() {
+  function createElements(elements) {
+    const trEl = document.createElement("tr");
+    trEl.classList.add("row");
+
+    const tdElCarMake = document.createElement("td");
+    tdElCarMake.value = elements.carMake.value;
+
+    const tdElCarModel = document.createElement("td");
+    tdElCarModel.value = elements.carModel.value;
+
+    const tdElProductionYear = document.createElement("td");
+    tdElProductionYear.value = elements.carProductionYear.value;
+
+    const tdElFuelType = document.createElement("td");
+    tdElFuelType.value = elements.carFuelType.value;
+
+    const tdElOriginalCost = document.createElement("td");
+    tdElOriginalCost.value = elements.originalCost.value;
+
+    const tdElSellingPrice = document.createElement("td");
+    tdElSellingPrice.value = elements.sellingPrice.value;
+
+    const tdElButtonsHolder = document.createElement("td");
+
+    const editButton = document.createElement("button");
+    editButton.classList.add("action-btn", "edit");
+
+    const sellButton = document.createElement("button");
+    sellButton.classList.add("action-btn", "sell");
+
+    appendElements();
+  }
+
+  function appendElements(elements, elementsToAppend) {}
+
+  function publishCar(elements) {}
+
   function eventHandling(elements) {}
 
   function gatherElements() {
@@ -11,6 +48,7 @@
     const originalCost = document.getElementById("original-cost");
     const sellingPrice = document.getElementById("selling-price");
     const tableBodyElement = document.getElementById("table-body");
+    const publishButton = document.getElementById("publish");
 
     return {
       carMake,
@@ -20,6 +58,7 @@
       originalCost,
       sellingPrice,
       tableBodyElement,
+      publishButton,
     };
   }
 
