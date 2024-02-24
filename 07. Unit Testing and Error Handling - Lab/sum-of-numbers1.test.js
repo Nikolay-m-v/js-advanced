66b6fb135a1958a6025160e4ca455e1a0d51ee0c
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-24 15:05:37 +0200

    doing old tasks to retest

--- 07. Unit Testing and Error Handling - Lab/sum-of-numbers1.test.js
+++ 07. Unit Testing and Error Handling - Lab/sum-of-numbers1.test.js
@@ -7,3 +7,7 @@ test(`the sum should be equal to all numbers from the array combined`, () => {
 test(`works with number array as string`, () => {
   expect(sum(["1", "2", "3"])).toBe(6);
 });
+
+test(`returns NaN when one of the parameters isnt a number/string(number)`, () => {
+  expect(sum(["a", "3", "6"])).toBe(NaN);
+});
