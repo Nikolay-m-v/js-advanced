ba5545f63775af3a221bbceebb47a82072b9cdf5
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-24 15:37:02 +0200

    doing old tasks to retest

---
+++ 07. Unit Testing and Error Handling - Lab/check-for-symmetry.test.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const isSymmetric = require("./check-for-symmetry");
+
+test(`Works with symmetric  numeric array`, () => {
+  expect(isSymmetric([1, 2, 3])).toBe(false);
+});
+
+test("empty array is symmetric", () => {
+  expect(isSymmetric([])).toBe(true);
+});
+
+test("single-element array is symmetric", () => {
+  expect(isSymmetric([1])).toBe(true);
+});
