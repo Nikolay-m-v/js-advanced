9d894238723616f002beac9e8b064e0397dc4c4c
Nikolay-m-v <nikolay.m.veselinov@gmail.com>
2024-02-28 12:30:42 +0200

    more jest test

--- 07. Unit Testing and Error Handling - Lab/addSubtract.test.js
+++ 07. Unit Testing and Error Handling - Lab/addSubtract.test.js
@@ -1,3 +1,13 @@
 "use strict";
 
+const createCalculator = require("./addSubtract");
 const addSubtract = require("./addSubtract");
+
+test("should return an object with add, subtract and get functions", () => {
+  const calculator = createCalculator();
+  expect(calculator).toMatchObject({
+    add: expect.any(Function),
+    subtract: expect.any(Function),
+    get: expect.any(Function),
+  });
+});
